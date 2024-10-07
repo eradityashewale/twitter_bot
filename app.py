b6b8633cc06cf653b7b19f860a5f8c93303eb3cf
Aditya Shewale <141516636+eradityashewale@users.noreply.github.com>
2024-10-07 17:26:49 +0530

    Close the welcome menu of Twitter

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.edge.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
 # Correct path to the ChromeDriver executable
@@ -16,12 +18,20 @@ edge_options = webdriver.EdgeOptions()
 driver = webdriver.Edge(service=service, options=edge_options)
 
 # Open Twitter
-web = 'https://twitter.com/'
+web = 'https://www.twitter.com'
 driver.get(web)
 
-# Find and click the login button
-login = driver.find_element(By.XPATH, "//a[@href='/login']")
-login.click()
+# Wait until the login button is clickable and click it
+try:
+    close = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, "//button[@class='css-175oi2r r-sdzlij r-1phboty r-rs99b7 r-lrvibr r-2yi16 r-1qi8awa r-3pj75a r-1loqt21 r-o7ynqc r-6416eg r-1ny4l3l']"))
+    )
+    close.click()
+except Exception as e:
+    print(f"Error occurred: {e}")
 
-# Sleep for 2 seconds to allow time for the next page to load
-time.sleep(2)
+# Optional: Add some wait time to see the result
+time.sleep(100)
+
+# Close the browser
+driver.quit()
